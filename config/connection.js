1bd16fed86b1bdeaa822b775c9a504ee44ae183b
daryllpenafiel <daryll_penafiel@yahoo.com>
2018-07-25 17:45:39 -0400

    fixed deployment issues

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,10 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "pack_db",
-  port: 5000
+  host: "us-cdbr-iron-east-04.cleardb.net",
+  user: "b01c4dd70b4708",
+  password: "93460106",
+  database: "heroku_1bd905f7a7a4b09",
 });
 
 connection.connect(function(err) {
